5b29b4cc88bdb449d3910efd6f73f1fceb4a6a57
Yichen <syichen@seas.upenn.edu>
2017-08-17 09:22:19 -0400

    8.17 (1)

--- microsoft.java
+++ microsoft.java
@@ -93,3 +93,46 @@ public RandomListNode copyRandomList(RandomListNode head) {
     }
     return newHead.next;
 }
+
+419. Battleships in a Board
+public int countBattleships(char[][] board) {
+    if (board == null || board.length == 0 || board[0].length == 0) return 0;
+    int count = 0;
+    for (int i = 0; i < board.length; i++) {
+        for (int j = 0; j < board[0].length; j++) {
+            if (board[i][j] == '.') continue;
+            if (i > 0 && board[i - 1][j] == 'X' || j > 0 && board[i][j - 1] == 'X') continue;
+            count++;
+        }
+    }
+    return count;
+}
+
+273. Integer to English Words
+public class Solution {
+    private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+        int i = 0;
+        String result = "";
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                result = helper(num % 1000) + THOUSANDS[i] + " " + result;
+            }
+            i++;
+            num /= 1000;
+        }
+        return result.trim();
+    }
+    private String helper(int num) {
+        if (num == 0) return "";
+        else if (num < 20) return LESS_THAN_20[num] + " ";
+        else if (num < 100) return TENS[num / 10] + " " + helper(num % 10);
+        else {
+            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+        }
+    }
+}
