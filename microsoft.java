70b53eaa27671c85aefa069ac22420c425325473
Yichen <syichen@seas.upenn.edu>
2017-08-23 22:27:02 -0400

    8.23 (1)

--- microsoft.java
+++ microsoft.java
@@ -821,3 +821,22 @@ private void inorder(TreeNode root, ArrayList<Integer> array) {
     array.add(root.val);
     inorder(root.right, array);
 }
+
+20. Valid Parentheses
+/* ")}" "([])" "()}" "((" */
+public boolean isValid(String s) {
+    if (s == null || s.length() == 0) return false;
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i);
+        if (c == '(') stack.push(')');
+        else if (c == '[') stack.push(']');
+        else if (c == '{') stack.push('}');
+        else if (c == ')' || c == ']' || c == '}') {
+            if (stack.isEmpty() || !stack.isEmpty() && stack.pop() != c) {
+                return false;
+            }
+        }
+    }
+    return stack.isEmpty();
+}
