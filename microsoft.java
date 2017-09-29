b0f143dce9761dfa53ea145dc29cb289d6ffc633
Yichen <syichen@seas.upenn.edu>
2017-09-29 10:24:25 -0400

    9.29

--- microsoft.java
+++ microsoft.java
@@ -2403,3 +2403,18 @@ class Solution {
         return dp[N];
     }
 }
+
+672. Bulb Switcher II
+class Solution {
+    public int flipLights(int n, int m) {
+        if (m == 0) return 1;
+        if (n == 1) return 2;
+        if (n == 2 && m == 1) return 3;
+        if (n == 2) return 4;
+
+        if (m == 1) return 4;
+        else if (m == 2) return 7;
+        else return 8;
+
+    }
+}
