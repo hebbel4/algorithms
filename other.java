383522a0a1dba33484bd882bf0e5d890129a3eae
Yichen <syichen@seas.upenn.edu>
2017-10-06 23:40:08 -0400

    10.6

--- other.java
+++ other.java
@@ -182,6 +182,21 @@ class Solution {
         return global[k];
     }
 }
+/* using tushar's */
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        if (prices.length == 0) return 0;
+        int[][] dp = new int[k + 1][prices.length];
+        for (int i = 1; i <= k; i++) {
+            int tempMax = -prices[0];
+            for (int j = 1; j < prices.length; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], prices[j] + tempMax);
+                tempMax = Math.max(tempMax, dp[i - 1][j] - prices[j]);
+            }
+        }
+        return dp[k][prices.length - 1];
+    }
+}
 
 65. Valid Number
 class Solution {
