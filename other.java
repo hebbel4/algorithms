2568a81fe1ca8d9a67c00c882239e551e04dafc0
Yichen <syichen@seas.upenn.edu>
2017-10-16 00:32:48 -0400

    10.16

--- other.java
+++ other.java
@@ -555,3 +555,18 @@ public class Solution {
 
     }
 }
+
+7. Reverse Integer
+class Solution {
+    public int reverse(int x) {
+        long result = 0;
+        while (x != 0) {
+            result = result * 10 + x % 10;
+            x = x / 10;
+            if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        return (int) result;
+    }
+}
